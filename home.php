a7e41cfa97a9b2e8ae4e0a11b82949098f65635e
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2024-12-14 23:36:41 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,19 +1,7 @@
 <?php
 session_start();
 
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "pharmaease_db";
-
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require 'includes/db_connect.php';
 
 
 // SQL query to fetch products
